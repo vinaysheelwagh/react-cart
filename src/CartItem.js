2b59c36c5b19e86e2afb84719ab578479d0a5de1
Vinaysheel Wagh <vinaysheel_wagh@hotmail.com>
2021-01-02 17:32:44 +0000

    changing api url
    Updated api url to get cart items

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useGlobalContext } from './context'
+
 const CartItem = ({ id, img, title, price, amount }) => {
   const {removeItem,increaseAmount, decreaseAmount} = useGlobalContext()
   return (
--- src/context.js
+++ src/context.js
@@ -1,7 +1,8 @@
 import React, { useState, useContext, useReducer, useEffect } from 'react'
 import cartItems from './data'
 import reducer from './reducer'
-const url = 'https://course-api.netlify.app/api/react-useReducer-cart-project'
+const url = 'https://course-api.com/react-useReducer-cart-project'
+
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
